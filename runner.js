f468d7abb796f8dd577e4e8bb3711cf596850323
Gabriel Rossi <gabriel.rossi@virtual360.io>
2020-05-06 22:46:21 -0300

    padovan series and test

--- runner.js
+++ runner.js
@@ -1,9 +1,14 @@
 const sequences = require('./sequences');
 const test = require('./test');
 
-const fibo = sequences.fibonacci
+const fibo = sequences.fibonacci;
+
+const pado = sequences.padovan;
 
 console.log(`For Fibonacci: ${test.doAsserts(fibo, test.fiboCases)}`);
+
+console.log(`For Padovan: ${test.doAsserts(pado, test.padoCases)}`);
+
 
 
 
--- sequences.js
+++ sequences.js
@@ -9,8 +9,18 @@ const sequenceFunctions = {
 			arr.push(arr[i-2] + arr[i-1]);
 		}
 		return arr[n];
+	},
+
+	padovan: (n) => {
+		const arr = [1,1,1];
+
+		for (let i = 3; i <= n; i++) {
+			arr.push(arr[i-3] + arr[i-2]);
+		}
+		return arr[n];
 	}
 }
+
 
 module.exports = sequenceFunctions ;
 
